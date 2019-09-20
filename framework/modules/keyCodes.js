0528bd6674d086cc24f6258d0a6c3e422eb86ded
RiggedToExplode <thomas.zent@gmail.com>
2019-09-20 16:05:24 -0700

    Abandon using modules

--- framework/modules/keyCodes.js
+++ framework/modules/keyCodes.js
@@ -1,1 +1,1 @@
-keyCodes=[],window.addEventListener("keydown",function(e){keyCodes[e.keyCode]=!0},!1),window.addEventListener("keyup",function(e){keyCodes[e.keyCode]=!1},!1);export default keyCodes;
+$P.keyCodes=[],window.addEventListener("keydown",function(e){keyCodes[e.keyCode]=!0},!1),window.addEventListener("keyup",function(e){keyCodes[e.keyCode]=!1},!1);
